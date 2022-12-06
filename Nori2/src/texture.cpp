192897d44dfcdb77b69ab23e6b1667714ad450d0
Mateo Vallejo <r3nstark@gmail.com>
2022-12-06 20:36:54 +0100

    rip nakagin

--- Nori2/src/texture.cpp
+++ Nori2/src/texture.cpp
@@ -23,62 +23,6 @@
 
 
 NORI_NAMESPACE_BEGIN
-class BitmapTexture: public Texture {
-public:
-	BitmapTexture(const PropertyList& props) {
-		m_bitmap = 0;
-
-		m_bitmap_name = props.getString("filename", "null");
-		filesystem::path filename =
-			getFileResolver()->resolve(m_bitmap_name);
-
-		std::ifstream is(filename.str());
-		if (!is.fail())
-		{
-			cout << "Loading Texture Map: " << filename.str() << endl;
-
-			m_bitmap = new LDRBitmap(filename.str());
-			cout << "Loaded " << m_bitmap_name << " - SIZE [" << m_bitmap->rows() << ", " << m_bitmap->cols() << "]" << endl;
-		}
-		m_color = props.getColor("color", Color3f(1.));
-		m_scale[0] = props.getFloat("scalex", 1.f);
-		m_scale[1] = props.getFloat("scaley", 1.f);
-	}
-	~BitmapTexture()
-	{
-		if (m_bitmap)
-			delete m_bitmap;
-
-		m_bitmap = 0;
-	}
-
-	virtual std::string toString() const {
-		return tfm::format(
-			"Texture[\n"
-			"  scale = %s,\n"
-			"  name = %s,\n"
-			"]",
-			m_color.toString(),
-			m_bitmap_name);
-	}
-
-	virtual Color3f eval(const Point2f& uv) const {
-
-		if (!m_bitmap)
-			return m_color;
-
-		return m_bitmap->eval(uv) * m_color;
-	}
-
-protected:
-	Color3f m_color;
-	LDRBitmap* m_bitmap;
-	float m_rotation;
-	Vector2f m_scale;
-
-	std::string m_bitmap_name;
-};
-
 
 class CheckerTexture : public Texture {
 public:
@@ -120,6 +64,6 @@ protected:
 };
 
 
-NORI_REGISTER_CLASS(BitmapTexture, "textmap")
+//NORI_REGISTER_CLASS(BitmapTexture, "textmap")
 NORI_REGISTER_CLASS(CheckerTexture, "checker")
 NORI_NAMESPACE_END
