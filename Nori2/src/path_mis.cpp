5bb70a7c1492bc6d9a10ed4e3ff26681318c40be
Óscar Pueyo Ciutad <oscar@graus.biz>
2022-12-08 18:11:35 +0100

    Fixed a lot of things RR on throughput

--- Nori2/src/path_mis.cpp
+++ Nori2/src/path_mis.cpp
@@ -8,21 +8,10 @@ NORI_NAMESPACE_BEGIN
 
 class PathTracingMIS : public Integrator {
 private:
-	struct LQueryRecord {
-		Color3f L;
-		bool lastRayHitdLight;
-		EMeasure pdfType;
 
-		LQueryRecord(Color3f _l, bool _lastRayHitdLight, EMeasure _pdfType) :
-				L(std::move(_l)), lastRayHitdLight(_lastRayHitdLight), pdfType(_pdfType) {}
-
-		bool needLightSample() {
-			return !(lastRayHitdLight || pdfType == EDiscrete);
-		}
-	};
-
-	static Color3f powerHeuristic(Color3f L, float p, float o) {
-		return p * L / (p + o);
+	static Color3f powerHeuristic(Color3f Lem, float p, float o) {
+		if (p + o == 0.0f) p = 1.0f;
+		return p * Lem / (p + o);
 	}
 
 public :
@@ -52,88 +41,89 @@ public :
 			pem *= pdflight;
 		}
 
-		float pmat = it.mesh->getBSDF()->pdf(BSDFQueryRecord(it.toLocal(-ray.d), it.toLocal(emitterRecord.wi), emitterRecord.uv, ESolidAngle));
-
-		// Prevent nans
-		if (pmat + pem == 0)  pmat = 1.0;
+		float pmat = it.mesh->getBSDF()->pdf(
+				BSDFQueryRecord(it.toLocal(-ray.d), it.toLocal(emitterRecord.wi),
+									emitterRecord.uv, ESolidAngle));
 
 		return powerHeuristic(Lem, pem, pmat);
 	}
 
-	std::tuple<LQueryRecord, bool> Lmat(const Scene* scene, Sampler* sampler, const Ray3f& ray, const Intersection& it, bool secondary) const {
-		BSDFQueryRecord bsdfRecord(it.toLocal(-ray.d), it.uv);
-		Color3f frcos = it.mesh->getBSDF()->sample(bsdfRecord, sampler->next2D());
-
-		// Russian Roulette
-		float k = frcos.getLuminance() > 0.9f ? 0.9f : frcos.getLuminance();
-		if (!secondary) k = 1.0;
-		if (sampler->next1D() > k) return {{0, false, EUnknownMeasure}, true};
-
-		// Get direction for new ray
-		Ray3f nray(it.p, it.toWorld(bsdfRecord.wo));
-		Intersection nit;
-
-		LQueryRecord nextRay = LiR(scene, sampler, nray);
-		Color3f Lmat = (frcos * nextRay.L) / k;
-
-		// If specular bounce not MIS
-		if (bsdfRecord.measure == EDiscrete) {
-			return {{Lmat, false, bsdfRecord.measure}, false};
-		}
-
-		/*
-		// Case last bounce or first
-		if (!secondary || nextRay.lastRayHitdLight) {
-			return {{Lmat, false, bsdfRecord.measure}, false};
-		}
-		 */
-
-		// Case not last bounce
-		float pmat = it.mesh->getBSDF()->pdf(bsdfRecord);
-
-		float pem = 0.0f;
-		bool hit = scene->rayIntersect(nray, nit);
-		// Get emitter pdf
-		if (hit && nit.mesh->isEmitter()) {
-			const Emitter* em = nit.mesh->getEmitter();
-			EmitterQueryRecord emitterRecordBSDF(em, it.p, nit.p, nit.shFrame.n, nit.uv);
-			pem = scene->pdfEmitter(em) * em->pdf(EmitterQueryRecord(em,it.p, nit.p, nit.shFrame.n, nit.uv));
-		}
-		// Prevent nans and combine with MIS
-		if (pmat + pem == 0.0f) pmat = 1.0f;
-		return {{powerHeuristic(Lmat, pmat, pem), false, bsdfRecord.measure}, true};
-
+	float pMatEm(const Scene* scene, const Intersection& it, const Intersection& nit) const {
+		const Emitter* em = nit.mesh->getEmitter();
+		if (em == nullptr) return 0.0f;
+		EmitterQueryRecord emitterRecordBSDF(em, it.p, nit.p, nit.shFrame.n, nit.uv);
+		return scene->pdfEmitter(em) * em->pdf(EmitterQueryRecord(em,it.p, nit.p, nit.shFrame.n, nit.uv));
 	}
 
-	LQueryRecord LiR(const Scene* scene, Sampler* sampler, const Ray3f& ray, bool secondary=true) const {
-		// Intersect with scene geometry
-		Intersection it;
-		if (!scene->rayIntersect(ray, it)) {
-			return {scene->getBackground(ray), true, EUnknownMeasure};
+	bool RR(Color3f& throughput, Sampler* sampler, bool& secondary, float maxRR=0.9f) const {
+		// RR with throughput instead of just fr
+		float k = throughput.getLuminance() > maxRR ? maxRR : throughput.getLuminance();
+		if (!secondary) {
+			k = 1.0;
+			secondary = true;
 		}
+		throughput /= k;
+		return sampler->next1D() > k;;
 
-		// Add light if intersected with emitter
-		if (it.mesh->isEmitter()) {
-			EmitterQueryRecord lightEmitterRecord(it.mesh->getEmitter(), ray.o, it.p, it.shFrame.n, it.uv);
-			return {it.mesh->getEmitter()->eval(lightEmitterRecord), true, EUnknownMeasure};
-		}
-
-		// MIS if not delta
-		auto [mat, needLight] = Lmat(scene, sampler, ray, it, secondary);
-		if (!needLight) return mat;
-
-		// Emitter sampling
-		Color3f emL = Lem(scene, sampler, ray, it);
-
-		return {mat.L + emL, false, mat.pdfType};
 	}
 
 	Color3f Li(const Scene* scene, Sampler* sampler, const Ray3f& ray) const {
-		return LiR(scene, sampler, ray, false).L;
+		Ray3f nray = ray;
+		Color3f throughput(1.0f);
+		Color3f L(0.0f);
+		bool secondary = true;
+		bool absorbRay = false;
+
+		// Intersect with scene geometry
+		Intersection it;
+		bool intersected = scene->rayIntersect(nray, it);
+
+		while (!absorbRay) {
+			if (!intersected) {
+				L += throughput * scene->getBackground(nray);
+				break;
+			} else if (it.mesh->isEmitter()) {
+				// Add light if intersected with emitter
+				EmitterQueryRecord lightEmitterRecord(it.mesh->getEmitter(), nray.o, it.p, it.shFrame.n, it.uv);
+				L += throughput * it.mesh->getEmitter()->eval(lightEmitterRecord);
+				break;
+			}
+
+			// BSDF Sampling
+			BSDFQueryRecord bsdfRecord(it.toLocal(-nray.d), it.uv);
+			Color3f currThroughput = throughput;
+			throughput *= it.mesh->getBSDF()->sample(bsdfRecord, sampler->next2D());
+
+			// Absorb ray
+			absorbRay = RR(throughput, sampler, secondary);
+
+			Intersection nit = it;
+			if (!absorbRay) {
+				// Get direction for new ray
+				nray = Ray3f(it.p, it.toWorld(bsdfRecord.wo));
+				intersected = scene->rayIntersect(nray, nit);
+
+				// Final ray, apply MIS
+				if ((intersected && nit.mesh->isEmitter()) && bsdfRecord.measure != EDiscrete) {
+					throughput *= powerHeuristic(Color3f(1.0f),
+					                             it.mesh->getBSDF()->pdf(bsdfRecord),
+					                             pMatEm(scene, it, nit));
+				}
+			}
+
+			bool useLightSampling = bsdfRecord.measure != EDiscrete;
+			if (useLightSampling) {
+				// Already MIS weighted
+				L += currThroughput * Lem(scene, sampler, nray, it);
+			}
+
+			it = nit;
+		}
+		return L;
 	}
 
 	std::string toString() const {
-		return "Path Tracer mis Integrator []" ;
+		return "Path Tracer MIS Integrator []" ;
 	}
 };
 
